9a58b9d0fe809cb990f8deb5b883d89cdbcaf804
Malcolm Jones <malcolm@adobe.com>
2017-07-30 14:55:30 -0400

    Chg: missing dotfiles

---
+++ dotfiles/bash.functions.d/make.aliases.sh
@@ -0,0 +1,9 @@
+# Make
+alias mk="make"
+alias mkc="make clean"
+alias mka="make all"
+alias mkd='make d'
+alias mkp='make python'
+alias mkt='make test'
+alias mkx='make && make clean'
+alias mkf='make clean && make && make hex && make writeflash'
